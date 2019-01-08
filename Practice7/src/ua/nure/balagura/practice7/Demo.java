df782df2512c53130632543213b2c938cf409aa8
Admin <Admin@NB>
2019-01-08 22:59:26 +0200

    practice7 init

---
+++ Practice7/src/ua/nure/balagura/practice7/Demo.java
@@ -0,0 +1,8 @@
+package ua.nure.balagura.practice7;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Main.main(new String[] { "input.xml"});
+	}
+}
---
+++ Practice7/src/ua/nure/balagura/practice7/Main.java
@@ -0,0 +1,7 @@
+package ua.nure.balagura.practice7;
+
+public class Main {
+public static void main(String[] args) {
+	
+}
+}
